0b0ce2e19242064defd4fabf63333ac97ab1c96c
thom budel <thombudel@gmail.com>
2017-03-27 17:02:41 +0200

    Added routes for: attend, sponsor, book in-company

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  get "book_in_company/home", :to => "book_in_company#home"
+
+  get "sponsor/home", :to => "sponsor#home"
+
+  get "attend/home", :to => "attend#home"
+
 end
