7d2e6a1606e7a6ba40fab1697fcb3c581d5a3bc7
Awadje <michaelawad85@gmail.com>
2017-03-28 10:12:55 +0200

    Fixed NoMethod error

--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Workshop, type: :model do
 describe "association" do
     let (:event) { create :event }
     it "should work with an associated event" do
-      should_belong_to :events
-      workshop.should have(:no).errors_on(:event)
+      workshop = event.workshops.new(name: "Test")
+      expect(workshop.event).to eq(event)
     end
   end
 end
