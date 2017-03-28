74023535e0030f4d6330ef27a9e1bfe378bd01d1
Awadje <michaelawad85@gmail.com>
2017-03-28 11:21:52 +0200

    Create events index view

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,11 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.first
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   root to: 'pages#homepage'
 
   devise_for :users
@@ -6,5 +7,14 @@ Rails.application.routes.draw do
   get "in_company", to:  "pages#incompany"
   get "sponsor", to:  "pages#sponsor"
   get "attend", to: "pages#attend"
+=======
+  root to: "events#index"
+
+
+  get "book_in_company/home", :to => "book_in_company#home"
+  get "sponsor/home", :to => "sponsor#home"
+  get "attend/home", :to => "attend#home"
+>>>>>>> Create events index view
+
 
 end
