31ec044357e27b0cd14a4c8adbac5cb1ed0e7beb
PauladO <pauladenouden@gmail.com>
2017-03-28 14:06:48 +0200

    not sure what changed

--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Workshop, type: :model do
   describe "association" do
-    let!(:event) { create :event }
+    let (:sponsor) { create :sponsor }
+    let!(:event1) { create :event, sponsor: sponsor }
 
     it "should work with an associated event" do
-      workshop = event.workshop.new(title: "Test")
+      workshop = event1.workshop.new(title: "Test")
       expect(workshop.event).to eq(event)
     end
   end
