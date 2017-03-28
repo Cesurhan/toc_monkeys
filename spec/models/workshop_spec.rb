682d5d56b15a8b09f75eb0157a4b74e5f781a941
Awadje <michaelawad85@gmail.com>
2017-03-28 09:43:17 +0200

    Grammar corrected

--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Workshop, type: :model do
 
 describe "association" do
     let (:event) { create :event }
-    it "should work with an accociated event" do
+    it "should work with an associated event" do
       should_belong_to :events
       workshop.should have(:no).errors_on(:event)
     end
