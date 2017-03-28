803b905be922e05d838f5080d4496ae259ec520a
Awadje <michaelawad85@gmail.com>
2017-03-28 16:10:24 +0200

    Removed not needed code/files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20170327135534) do
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,8 +62,11 @@ ActiveRecord::Schema.define(version: 20170327135534) do
     t.string   "link_to_github"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.integer  "event_id"
+    t.index ["event_id"], name: "index_workshops_on_event_id", using: :btree
   end
 
   add_foreign_key "events", "sponsors"
   add_foreign_key "events", "workshops"
+  add_foreign_key "workshops", "events"
 end
