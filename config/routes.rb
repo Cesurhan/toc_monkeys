4bd4c549e0603f1e469b5269663566c55d727a95
Awadje <michaelawad85@gmail.com>
2017-03-29 17:14:59 +0200

    removed files created for render

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
- root to: 'pages#homepage'
-
- devise_for :users
-
- get "book_in_company/home", :to => "pages#incompany"
-
- get "sponsor/home", :to => "pages#sponsor"
-
- get "attend/home", :to => "pages#public"
-
+   root to: 'pages#homepage'
+   devise_for :users
+   get "book_in_company/home", :to => "book_in_company#home"
+   get "sponsor/home", :to => "sponsor#home"
+   get "attend/home", :to => "attend#home"
 end
