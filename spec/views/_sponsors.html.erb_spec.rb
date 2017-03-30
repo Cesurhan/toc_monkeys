2f81f4ce601681bfe5720093f68bb14f77994f3d
Awadje <michaelawad85@gmail.com>
2017-03-30 11:42:24 +0200

    Create tests for different pages loading partial

--- spec/views/_sponsors.html.erb_spec.rb
+++ spec/views/_sponsors.html.erb_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "shared/sponsors.html.erb", type: :view do
+describe "shared/sponsors.html.erb", type: :view do
 
   it "contains Attend text" do
     render 'shared/sponsors.html.erb'
     expect(rendered).to have_content("Proudly")
   end
+
+  it "contains Incompany text" do
+    render 'shared/sponsors.html.erb'
+    expect(rendered).to have_content("Proudly")
+  end
+
+  it "contains Sponsor text" do
+    render 'shared/sponsors.html.erb'
+    expect(rendered).to have_content("Proudly")
+  end
+
 
 end
