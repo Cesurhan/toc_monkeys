37158e384797ea45a541e5032b3f0d2131971d4e
thom budel <thombudel@gmail.com>
2017-03-30 17:26:59 +0200

    Fixed comments

--- spec/views/pages/incompany.html.erb_spec.rb
+++ spec/views/pages/incompany.html.erb_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
+RSpec.describe "pages/incompany.html.erb",  type: :view do
 
-RSpec.describe "Render workshops" do
-  let(:workshop1) { create :workshop, title: "Cool Workshop" }
-
-
-  it "shows workshop" do
-
-    expect(workshop1).to include("Cool Workshop")
-
-  end
-end
+      it "Should render choose your course section" do
+        render 
+        expect(render).to have_text("Choose your course")
+      end
+    end
