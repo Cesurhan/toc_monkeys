e83aad1d8e2297a12f5590ca29940fb8c54c50d9
PauladO <pauladenouden@gmail.com>
2017-03-30 09:53:00 +0200

    create cost calculation slider

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,32 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// function toggleCost() {
+//   $(this).toggleClass("selected-cost-btn");
+//   // var
+//   // updateCost();
+// }
+//
+// function updateCost() {
+//   var costs = [ "0 EUR", "2500 EUR", "5000 EUR", "7500 EUR", "10000 EUR" ];
+//   $("#eventCost").html($(this).id());
+//
+// }
+//
+//
+// $(document).ready(function() {
+//   $(".participant-count-btn").bind('click', toggleCost);
+//   // updateCost();
+// });
+
+
+$( function() {
+   $( "#slider-range-max" ).slider({
+     range: "max",
+     min: 0,
+     max: 80,
+     value: 20,
+     slide: function( event, ui ) {
+       $( "#amount" ).val( ui.value );
+       $("#cost").html( ui.value * 125 + " EUR")
+     }
+   });
+   $( "#amount" ).val( $( "#slider-range-max" ).slider( "value" ) );
+ } );
