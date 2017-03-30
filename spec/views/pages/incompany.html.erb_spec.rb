3b4ed476f292a9f7d7b0c2c4172d763aec38a1df
thom budel <thombudel@gmail.com>
2017-03-30 13:18:32 +0200

    Added test

--- spec/views/pages/incompany.html.erb_spec.rb
+++ spec/views/pages/incompany.html.erb_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "pages/incompany.html.erb",  type: :view do
+RSpec.describe "Render workshops" do
+  let(:workshop1) { create :workshop, title: "Cool Workshop" }
 
-    it "Should render choose your course section" do
-      render "shared/choose_your_course.html.erb"
 
-      response.should render_template(partial: '_choose_your_course.html.erb')
+  it "shows workshop" do
 
-    end
+    expect(workshop1).to include("Cool Workshop")
+
   end
+end
