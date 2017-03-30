f0984332cb3b2739f26f33fd6cbcd337ba012209
thom budel <thombudel@gmail.com>
2017-03-30 18:02:41 +0200

    Changes

--- spec/views/pages/attend.html.erb_spec.rb
+++ spec/views/pages/attend.html.erb_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe "pages/attend.html.erb",  type: :view do
 
     it "Should render get ready for day section" do
-      render "shared/get_ready_for_day.html.erb"
-
+      render 
       expect(rendered).to have_content("Get Ready for the Day")
     end
   end
