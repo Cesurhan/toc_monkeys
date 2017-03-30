7aaf3aa7b20f1a326e1d8d51155072d2d760d5bc
thom budel <thombudel@gmail.com>
2017-03-30 11:11:45 +0200

    Added test for rendering partial on page

---
+++ spec/views/pages/attend.html.erb_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "pages/attend.html.erb",  type: :view do
+
+    it "Should render get ready for day section" do
+      render "shared/get_ready_for_day.html.erb"
+
+      expect(rendered).to render_template(partial: '_get_ready_for_day.html.erb')
+    end
+  end
