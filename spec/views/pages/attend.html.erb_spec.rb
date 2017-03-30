84bc66c812f02687171ae42ba225ce8aa4c5d94e
thom budel <thombudel@gmail.com>
2017-03-30 11:19:37 +0200

    Editd test

--- spec/views/pages/attend.html.erb_spec.rb
+++ spec/views/pages/attend.html.erb_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe "pages/attend.html.erb",  type: :view do
     it "Should render get ready for day section" do
       render "shared/get_ready_for_day.html.erb"
 
-      expect(rendered).to render_template(partial: '_get_ready_for_day.html.erb')
+      expect(rendered).to have_content("Get Ready for the Day")
     end
   end
