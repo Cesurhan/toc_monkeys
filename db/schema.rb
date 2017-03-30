796eb759566acfd905e6eefea415aa5648e454cc
Cesurhan <ces-ur@hotmail.com>
2017-03-30 11:19:17 +0200

    Add test for newsletter partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327135534) do
+ActiveRecord::Schema.define(version: 20170327150355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170327135534) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
 
   create_table "workshops", force: :cascade do |t|
     t.string   "title"
@@ -60,8 +61,11 @@ ActiveRecord::Schema.define(version: 20170327135534) do
     t.string   "link_to_github"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.integer  "event_id"
+    t.index ["event_id"], name: "index_workshops_on_event_id", using: :btree
   end
 
   add_foreign_key "events", "sponsors"
   add_foreign_key "events", "workshops"
+  add_foreign_key "workshops", "events"
 end
