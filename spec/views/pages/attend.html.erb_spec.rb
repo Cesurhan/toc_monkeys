7b754be345a6ffc903d1cd7367261c1e132e17cb
thom budel <thombudel@gmail.com>
2017-03-30 17:58:50 +0200

    Changed test

--- spec/views/pages/attend.html.erb_spec.rb
+++ spec/views/pages/attend.html.erb_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe "pages/attend.html.erb",  type: :view do
 
     it "Should render what you will learn section" do
-      render "shared/what_you_will_learn.html.erb"
-
+      render 
       expect(rendered).to have_content("What You Will Learn")
     end
   end
