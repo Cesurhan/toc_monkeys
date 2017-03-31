a3c03de1365a196efcc2c7f9f76e5c94f6e13534
thom budel <thombudel@gmail.com>
2017-03-31 11:49:53 +0200

    Added sponsorrequest model

---
+++ app/models/sponsorrequest.rb
@@ -0,0 +1,2 @@
+class Sponsorrequest < ApplicationRecord
+end
---
+++ spec/factories/sponsorrequests.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :sponsorrequest do
+    first_name "MyString"
+    last_name "MyString"
+    company_name "MyString"
+    email "MyString"
+    date "2017-03-31"
+    spaces_available 1
+    comments "MyText"
+  end
+end
