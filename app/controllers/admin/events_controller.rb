38ec1804bb4b79e8df81ba7ffa876cf972a7d470
PauladO <pauladenouden@gmail.com>
2017-03-31 11:44:12 +0200

    Add show page and show controller

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,5 +1,30 @@
 class Admin::EventsController < ApplicationController
+  before_action :set_event, only: [:show, :destroy]
+
   def index
     @events = Event.all
   end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to admin_events_path
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+    end
 end
