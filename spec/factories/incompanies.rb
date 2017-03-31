c84a01693bd56eb49f53fca4da343d1a40c09b3b
Tenicia <tenicia@gmail.com>
2017-03-31 14:16:26 +0200

    --wip-- [skip ci]

---
+++ spec/factories/incompanies.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :incompany do
+    first_name "MyString"
+    last_name "MyString"
+    company_name "MyString"
+    email "MyString"
+    date "2017-03-30"
+    spaces_available 1
+    comments "MyString"
+  end
+end
