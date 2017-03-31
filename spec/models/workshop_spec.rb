b2031a4f0e1f1a9ab19f32c0e88fc08858987fa1
PauladO <pauladenouden@gmail.com>
2017-03-31 11:50:24 +0200

    Update workshop_spec.rb
    removed space

--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Workshop, type: :model do
   describe "association" do
-    let (:sponsor) { create :sponsor }
+    let(:sponsor) { create :sponsor }
     let(:event1) { create :event, sponsor: sponsor }
 
     it "should work with an associated event" do
