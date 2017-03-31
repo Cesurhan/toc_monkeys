955418400a2f02da62cb69485943b617c33013ee
PauladO <pauladenouden@gmail.com>
2017-03-31 15:18:22 +0200

    fix form, add test

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,10 +2,9 @@ class Event < ApplicationRecord
   belongs_to :workshop, optional: true
   belongs_to :sponsor
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 30 }
   validates :location, presence: true
-  validates :spaces_available, presence:true
-
+  validates :spaces_available, numericality: { only_integer: true, greater_than: 0 }
 
   def self.public_events
     self.select { |event| event.status }
